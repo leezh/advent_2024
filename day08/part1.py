a6ddf2e84a85b6630e9a440dccc6c54b137d67ad
Zher Huei Lee <lee.zh.92@gmail.com>
2024-12-09 14:59:13 +0800

    Day 08 entry

---
+++ day08/part1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+#
+# Entry for https://adventofcode.com/2024/day/8
+
+import dataclasses
+import itertools
+import sys
+
+filename = sys.argv[1] if len(sys.argv) == 2 else "sample.txt"
+
+
+@dataclasses.dataclass
+class Coord:
+    x: int
+    y: int
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __neg__(self):
+        return Coord(-self.x, -self.y)
+
+    def __add__(a, b):
+        return Coord(a.x + b.x, a.y + b.y)
+
+    def __sub__(a, b):
+        return Coord(a.x - b.x, a.y - b.y)
+
+    def in_bounds(self, rows, cols):
+        return 0 <= self.x < cols and 0 <= self.y < rows
+
+
+rows = 0
+cols = 0
+antennas = {}
+with open(filename, "r") as f:
+    for line in f.readlines():
+        line = line.strip()
+        if not line:
+            continue
+        cols = 0
+        for c in line:
+            coord = Coord(cols, rows)
+            if c.isalnum():
+                if c not in antennas:
+                    antennas[c] = []
+                antennas[c].append(coord)
+            cols += 1
+        rows += 1
+
+resonances = set()
+for freq, coords in antennas.items():
+    for a, b in itertools.combinations(coords, 2):
+        antinode_a = a + (a - b)
+        if antinode_a.in_bounds(rows, cols):
+            resonances.add(antinode_a)
+        antinode_b = b + (b - a)
+        if antinode_b.in_bounds(rows, cols):
+            resonances.add(antinode_b)
+
+print(len(resonances))
